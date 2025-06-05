a9ba80d5c7fabf4d83d4b166b0281e1570088a90
Paul Campbell <98130346+pau1campbe11@users.noreply.github.com>
2025-06-05 10:58:41 +0100

    Update 5. add_depth_header.sh

--- 5. add_depth_header.sh
+++ 5. add_depth_header.sh
@@ -1,1 +1,18 @@
+#!/bin/bash -l
+
+################ SLURM SETTINGS ################
+#SBATCH --account=project0005             
+#SBATCH --job-name=add_depth_header
+#SBATCH --output=%x-%J.out                
+#SBATCH --error=%x-%J.err               
+#SBATCH --time=0-01:00:00               
+#SBATCH --mem=5G                         
+#SBATCH --ntasks=1                        
+#SBATCH --cpus-per-task=1                 
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=p.campbell.5@research.gla.ac.uk
+#SBATCH --mail-type=ALL
+
+################ RUN SCRIPT #################
+
 
