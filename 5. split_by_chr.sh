deb787a70bc8237c520bf70fed2a825b4dedb061
Paul Campbell <98130346+pau1campbe11@users.noreply.github.com>
2025-06-05 11:02:09 +0100

    Update 5. split_by_chr.sh

--- 5. split_by_chr.sh
+++ 5. split_by_chr.sh
@@ -1,1 +1,14 @@
+################ SLURM SETTINGS ################
+#SBATCH --account=project0005             
+#SBATCH --job-name=split_by_chr
+#SBATCH --output=%x-%J.out                
+#SBATCH --error=%x-%J.err               
+#SBATCH --time=0-01:00:00               
+#SBATCH --mem=5G                         
+#SBATCH --ntasks=1                        
+#SBATCH --cpus-per-task=1                 
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=p.campbell.5@research.gla.ac.uk
+#SBATCH --mail-type=ALL
 
+################ RUN SCRIPT #################
