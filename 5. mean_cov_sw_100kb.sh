623a64ba8680182ce6604da24dbf52bfd095d6a1
Paul Campbell <98130346+pau1campbe11@users.noreply.github.com>
2025-06-05 11:00:46 +0100

    Update 5. mean_cov_sw_100kb.sh

--- 5. mean_cov_sw_100kb.sh
+++ 5. mean_cov_sw_100kb.sh
@@ -1,1 +1,14 @@
+################ SLURM SETTINGS ################
+#SBATCH --account=project0005             
+#SBATCH --job-name=mean_cov_sw_100kb
+#SBATCH --output=%x-%J.out                
+#SBATCH --error=%x-%J.err               
+#SBATCH --time=0-01:00:00               
+#SBATCH --mem=5G                         
+#SBATCH --ntasks=1                        
+#SBATCH --cpus-per-task=1                 
+#SBATCH --ntasks-per-node=1
+#SBATCH --mail-user=p.campbell.5@research.gla.ac.uk
+#SBATCH --mail-type=ALL
 
+################ RUN SCRIPT #################
